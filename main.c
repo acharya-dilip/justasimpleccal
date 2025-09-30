a223d4e8b960a2f71b116b1fd4f8c99a8c9c1d0c
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-09-30 17:29:08 +0545

    moved the buttons downwards by one row

--- main.c
+++ main.c
@@ -37,71 +37,78 @@ static void activate(GtkApplication *app, gpointer user_data) {
     numgrid=gtk_grid_new(); //Initializing the Grid
     gtk_window_set_child (GTK_WINDOW (calculator),numgrid);//Setting the grid as a child of the windows or subwindow
 
-
-    //Initializing the button for 1
-    numbut1 = gtk_button_new_with_label("1");
-    //Placing the numbut1 on the grid
-    gtk_grid_attach(GTK_GRID(numgrid),numbut1,0,0,1,1);
-
-    //Initialising the button for 2
-    numbut2 = gtk_button_new_with_label("2");
-    //placing the numbut2 on the grid
-    gtk_grid_attach(GTK_GRID(numgrid),numbut2,1,0,1,1);
-
-    //Initialising the button for 3
-    numbut3 =gtk_button_new_with_label("3");
-    //placing the numbut3 on the grid
-    gtk_grid_attach(GTK_GRID(numgrid),numbut3,2,0,1,1);
-
-    //Initialising the button for 4
-    numbut4 = gtk_button_new_with_label("4");
-    //placing the numbut4 on the grid
-    gtk_grid_attach(GTK_GRID(numgrid),numbut4,0,1,1,1);
-
-    //Intialising the button for 5
-    numbut5 =gtk_button_new_with_label("5");
-    //placing the numbut5 on the grid
-    gtk_grid_attach(GTK_GRID(numgrid),numbut5,1,1,1,1);
-
-    //Intialising the button for 6
-    numbut6 =gtk_button_new_with_label("6");
-    //placing the numbut6 on the grid
-    gtk_grid_attach(GTK_GRID(numgrid),numbut6,2,1,1,1);
-
-    //Initialising the button for 7
-    numbut7 = gtk_button_new_with_label("7");
-    //placing the numbut7 on the grid
-    gtk_grid_attach(GTK_GRID(numgrid),numbut7,0,2,1,1);
-
-    //Initialising the button for 8
-    numbut8 = gtk_button_new_with_label("8");
-    //placing the numbut8 on the grid
-    gtk_grid_attach(GTK_GRID(numgrid),numbut8,1,2,1,1);
-
-    //Initialsing the button for 9
-    numbut9 = gtk_button_new_with_label("9");
-    //placing the numbut9 on the grid
-    gtk_grid_attach(GTK_GRID(numgrid),numbut9,2,2,1,1);
-
-    //Intialising the button for 0
-    numbut0 = gtk_button_new_with_label("0");
-    //placing the numbut0 on the grid
-    gtk_grid_attach(GTK_GRID(numgrid),numbut0,1,3,1,1);
-
-    //Initialising the button for decimal 9 ( . )
-    decimalbut = gtk_button_new_with_label(".");
-    //Placing the decimalbut on the grid
-    gtk_grid_attach(GTK_GRID(numgrid),decimalbut,0,3,1,1);
-
-    //Intialising the button to clear the display
-    percentbut = gtk_button_new_with_label("%");
-    //Placing the clear button on the grid
-    gtk_grid_attach(GTK_GRID(numgrid),percentbut,2,3,1,1);
-
+    //BASIC ARITHMATIC OPERATORS
     //Initiliasising the button for addition
     addbut = gtk_button_new_with_label("+");
     //Placing the addition button on the grid
     gtk_grid_attach(GTK_GRID(numgrid),addbut,3,0,1,1);
+
+
+    //THE NUMBER BUTTONS
+
+    //Initializing the button for 1
+    numbut1 = gtk_button_new_with_label("1");
+    //Placing the numbut1 on the grid
+    gtk_grid_attach(GTK_GRID(numgrid),numbut1,0,1,1,1);
+
+    //Initialising the button for 2
+    numbut2 = gtk_button_new_with_label("2");
+    //placing the numbut2 on the grid
+    gtk_grid_attach(GTK_GRID(numgrid),numbut2,1,1,1,1);
+
+    //Initialising the button for 3
+    numbut3 =gtk_button_new_with_label("3");
+    //placing the numbut3 on the grid
+    gtk_grid_attach(GTK_GRID(numgrid),numbut3,2,1,1,1);
+
+    //Initialising the button for 4
+    numbut4 = gtk_button_new_with_label("4");
+    //placing the numbut4 on the grid
+    gtk_grid_attach(GTK_GRID(numgrid),numbut4,0,2,1,1);
+
+    //Intialising the button for 5
+    numbut5 =gtk_button_new_with_label("5");
+    //placing the numbut5 on the grid
+    gtk_grid_attach(GTK_GRID(numgrid),numbut5,1,2,1,1);
+
+    //Intialising the button for 6
+    numbut6 =gtk_button_new_with_label("6");
+    //placing the numbut6 on the grid
+    gtk_grid_attach(GTK_GRID(numgrid),numbut6,2,2,1,1);
+
+    //Initialising the button for 7
+    numbut7 = gtk_button_new_with_label("7");
+    //placing the numbut7 on the grid
+    gtk_grid_attach(GTK_GRID(numgrid),numbut7,0,3,1,1);
+
+    //Initialising the button for 8
+    numbut8 = gtk_button_new_with_label("8");
+    //placing the numbut8 on the grid
+    gtk_grid_attach(GTK_GRID(numgrid),numbut8,1,3,1,1);
+
+    //Initialsing the button for 9
+    numbut9 = gtk_button_new_with_label("9");
+    //placing the numbut9 on the grid
+    gtk_grid_attach(GTK_GRID(numgrid),numbut9,2,3,1,1);
+
+    //Intialising the button for 0
+    numbut0 = gtk_button_new_with_label("0");
+    //placing the numbut0 on the grid
+    gtk_grid_attach(GTK_GRID(numgrid),numbut0,1,4,1,1);
+
+    //OTHER MISC BUTTONS
+
+    //Initialising the button for decimal ( . )
+    decimalbut = gtk_button_new_with_label(".");
+    //Placing the decimalbut on the grid
+    gtk_grid_attach(GTK_GRID(numgrid),decimalbut,0,4,1,1);
+
+    //Intialising the button to clear the display
+    percentbut = gtk_button_new_with_label("%");
+    //Placing the clear button on the grid
+    gtk_grid_attach(GTK_GRID(numgrid),percentbut,2,4,1,1);
+
+
 
 
 }
